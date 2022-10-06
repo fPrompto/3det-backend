0d8995e8d000870e9a3a6088607a0b4c920e9021
fprompto <fernando.monteiro.f11@gmail.com>
2022-10-06 15:21:36 -0300

    :sparkles: feat: Comando "test" modificado

--- src/commands/test.js
+++ src/commands/test.js
@@ -2,6 +2,8 @@ import { readFile } from '../utils/readWriteFile.js';
 
 const test = async () => {
   const testFile = await readFile('src/data/testMessage.json');
+  console.log('============== TEST ==============')
+  console.log(testFile);
 
   return { message: testFile };
 };
